d5914698de03e79ef3f83e690aa225b885860cba
greenballoon <1033116249@qq.com>
2019-11-26 16:19:01 +0800

    修改main方法 添加fun1方法

--- src/com/shop/web/test01.java
+++ src/com/shop/web/test01.java
@@ -3,5 +3,11 @@ package com.shop.web;
 public class test01 {
      public static void main(String[] args) {
              System.out.println("test01");
+
+             System.out.println("test02");
+      }
+      public void fun1()
+      {
+          System.out.println("fun1……");
       }
 }
